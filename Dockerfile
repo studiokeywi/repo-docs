fbb3f6acd5fca4f2a4722abf6e7e3a48e632801b
studioKeywi <github@studiokeywi.dev>
2024-05-03 08:48:18 -0500

    the ultimate change

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM       squidfunk/mkdocs-material:latest
 
 ENV        ENV="/root/.profile"
-WORKDIR    /root
-COPY       ./.profile ./
 WORKDIR    /docs
 COPY       ./action.sh ./mkdocs.root.yml ./
 RUN        pip install mkdocs-typedoc && chmod +x ./action.sh
--- action.sh
+++ action.sh
@@ -2,7 +2,8 @@
 
 apk add --no-cache libstdc++ coreutils curl bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash 
-. /root/.profile
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install --lts
 nvm use --lts
 apk del coreutils curl bash
