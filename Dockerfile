0662ecf82d0c5fd4f5b175d754b7a5c86d206eed
studioKeywi <github@studiokeywi.dev>
2024-05-03 10:03:10 -0500

    Updates
    - add .gitignore
    - install revision plugin

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ FROM       squidfunk/mkdocs-material:latest
 ENV        ENV="/root/.profile"
 WORKDIR    /docs
 COPY       ./action.sh ./mkdocs.root.yml ./
-RUN        pip install mkdocs-typedoc && chmod +x ./action.sh
+RUN        pip install mkdocs-typedoc mkdocs-git-revision-date-localized-plugin && chmod +x ./action.sh
 ENTRYPOINT cd /docs && sh ./action.sh
--- action.sh
+++ action.sh
@@ -3,9 +3,9 @@
 apk add --no-cache libstdc++ coreutils curl bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 export NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release
-nvm_get_arch() { nvm_echo "x64-musl"; }  # oh boy i hope this line doesn't break shit
+nvm_get_arch() { nvm_echo "x64-musl"; }
 nvm install --lts
 nvm use --lts
 cp -R /docs/* /github/workspace
