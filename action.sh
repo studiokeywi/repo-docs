30104e15337ae1bcd9f57dc23f9860acc5707a7b
studioKeywi <github@studiokeywi.dev>
2024-05-03 08:51:39 -0500

    forgot about the special .profile stuff

--- action.sh
+++ action.sh
@@ -4,9 +4,10 @@ apk add --no-cache libstdc++ coreutils curl bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+export NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release
+nvm_get_arch() { nvm_echo "x64-musl"; }  # oh boy i hope this line doesn't break shit
 nvm install --lts
 nvm use --lts
-apk del coreutils curl bash
 cp -R /docs/* /github/workspace
 cd /github/workspace
 npm i
